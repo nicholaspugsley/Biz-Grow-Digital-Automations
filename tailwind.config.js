7126bb95aac4e0ea6a83d2e69da2df2a289c845d
nicholaspugsley <nkpcoding@gmail.com>
2024-03-09 12:27:21 -0500

    another go

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,11 @@
 module.exports = {
   content: [
+    "./*.{html,js}", // Adjusted to include HTML and JS files under the pages directory
     "./pages/**/*.{html,js}", // Adjusted to include HTML and JS files under the pages directory
     "./assets/js/**/*.{html,js}", // Specifically targeting JS files in the assets/js directory
   ],
   theme: {
-    extend: {
-      colors: {
-        'light-blue': '#ebf8ff', // Light blue for hover states
-        'aquatic-blue': '#0077CC',
-        'shrimp-red': '#FF4136',
-        'seaweed-green': '#2ECC40',
-        'sandy-yellow': '#FFDC00',
-        'off-white': '#F0F0F0',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
